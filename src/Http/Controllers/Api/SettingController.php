6c97091af37fe2ae2dba727db6aee91a55d75c8b
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-03-15 09:56:52 +0700

    feat(api): enhance index method to apply filters for setting keys

--- src/Http/Controllers/Api/SettingController.php
+++ src/Http/Controllers/Api/SettingController.php
@@ -7,17 +7,20 @@ use Juzaweb\Modules\Core\Facades\Setting;
 
 class SettingController extends Controller
 {
-    public function index()
+    public function index(): \Illuminate\Http\JsonResponse
     {
-        $keys = [
-            'title',
-            'description',
-            'sitename',
-            'logo',
-            'favicon',
-            'banner',
-            'language',
-        ];
+        $keys = apply_filters(
+            'jw_api_setting_keys',
+            [
+                'title',
+                'description',
+                'sitename',
+                'logo',
+                'favicon',
+                'banner',
+                'language',
+            ]
+        );
 
         return response()->json(Setting::gets($keys));
     }
